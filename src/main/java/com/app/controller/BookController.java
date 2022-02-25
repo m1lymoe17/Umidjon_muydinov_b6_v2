bdbb7f4249c8d223c3d099c3becc8430afa2baaf
m1lymoe17 <muydinovumid2005@gmail.com>
2022-02-25 18:36:59 +0500

    save

--- src/main/java/com/app/controller/BookController.java
+++ src/main/java/com/app/controller/BookController.java
@@ -2,7 +2,10 @@ package com.app.controller;
 
 
 import com.app.model.Book;
+import com.app.model.User;
+import com.app.repository.UserRepository;
 import com.app.service.UserService.BookService;
+import com.app.service.UserService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +19,10 @@ import java.util.List;
 public class BookController {
     @Autowired
     BookService bookService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/get-save/{userId}")
     public String getSaveForm(@PathVariable(required = false, name = "userId") Integer userId, Model model) {
@@ -37,18 +44,18 @@ public class BookController {
         model.addAttribute("bookList", bookList);
         String save = bookService.getSave(book);
         model.addAttribute("save", save);
-
         return "home";
     }
 
     @RequestMapping("/delete/{id}")
-    public String getDelete(int id, Model model) {
+    public String getDelete(@PathVariable int id, Model model) {
         Book bookById = bookService.getBookById(id);
         int userId = bookById.getUserId();
+        User userById = userRepository.getUserById(userId);
         List<Book> bookList = bookService.getBookList(userId);
         model.addAttribute("bookList", bookList);
         String deleteBook = bookService.getDeleteBook(id);
-        model.addAttribute("msg", deleteBook);
+        model.addAttribute("user", userById);
         return "home";
     }
 }
